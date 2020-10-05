824a622d1289265eb165908ca6e32de27739fc73
bridget462 <bridges.dev.create@gmail.com>
2020-10-05 19:19:23 +0900

    server hello world with http module

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-console.log("hi");
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  console.log("methods", req.methods);
+  console.log("url", req.url);
+
+  const user = {
+    name: "John",
+    hobby: "Skating",
+  };
+
+  res.setHeader("Content-Type", "application/json");
+  res.end(JSON.stringify(user));
+});
+
+server.listen(3000);
