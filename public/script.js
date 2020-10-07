c9461364b9deb23f2183c9f0ef072a1556e1c8ca
bridget462 <bridges.dev.create@gmail.com>
2020-10-07 11:03:08 +0900

    peer user id tested

--- public/script.js
+++ public/script.js
@@ -4,8 +4,11 @@ const myPeer = new Peer(undefined, {
   port: "3001",
 });
 
-// calling socket event listener defined at server.js
-socket.emit("join-room", ROOM_ID, 10);
+// when connecting peer server, get generated unique id and do things
+myPeer.on("open", (id) => {
+  // calling socket event listener defined at server.js
+  socket.emit("join-room", ROOM_ID, id);
+});
 
 socket.on("user-connected", (userId) => {
   console.log(`user connected: ${userId}`);
