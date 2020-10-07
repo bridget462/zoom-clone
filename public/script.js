34b1e71eafdeea463147bbb3889d3c832d20bcde
bridget462 <bridges.dev.create@gmail.com>
2020-10-07 11:09:34 +0900

    video stream tested

--- public/script.js
+++ public/script.js
@@ -4,6 +4,20 @@ const myPeer = new Peer(undefined, {
   port: "3001",
 });
 
+const videoGrid = document.getElementById("video-grid");
+const myVideo = document.createElement("video");
+// mute ourself
+myVideo.muted = true;
+
+navigator.mediaDevices
+  .getUserMedia({
+    video: true,
+    audio: true,
+  })
+  .then((stream) => {
+    addVideoStream(myVideo, stream);
+  });
+
 // when connecting peer server, get generated unique id and do things
 myPeer.on("open", (id) => {
   // calling socket event listener defined at server.js
@@ -13,3 +27,11 @@ myPeer.on("open", (id) => {
 socket.on("user-connected", (userId) => {
   console.log(`user connected: ${userId}`);
 });
+
+function addVideoStream(video, stream) {
+  video.srcObject = stream;
+  video.addEventListener("loadedmetadata", () => {
+    video.play();
+  });
+  videoGrid.append(video);
+}
